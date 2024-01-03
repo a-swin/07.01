9bc2717c804870d4dcedc3dfcbf889d49180829b
Kathir-S-10 <droptokathir@gmail.com>
2024-01-03 12:46:47 +0530

    new change 3jan

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,6 +14,13 @@ import Members from './Components/Members';
 import Login from './Components/Login';
 import Sign from './Components/Sign';
 
+import dashboard from './Components/Images/dashboard (2).svg'
+import project from './Components/Images/priority (1).svg'
+import member from './Components/Images/peoples.svg'
+import setting from './Components/Images/settings (1).svg'
+import calendars from './Components/Images/calendar (1).svg'
+import { People } from '@mui/icons-material';
+
 function Navbar() {
   const [open, setOpen] = useState(false);
 
@@ -28,16 +35,16 @@ function Navbar() {
 
     <div>
       <div style={{ backgroundColor: " rgba(120, 136, 255, 0.938);" }}>
-        <AppBar position="fixed" className='appbar'>
+        <AppBar position="fixed" className='appbars'>
           <Toolbar className='appbar'>
             <IconButton edge="start" color="inherit" onClick={toggleDrawer} sx={{ mr: 2 }}>
               <MenuIcon />
             </IconButton>
-            <div className='logo'>
+            <div className='logos'>
               <h5>HR Tool</h5>
 
               <div >
-              <TextField id="standard-basic" placeholder=' Search' variant="standard" className='search' />
+              <TextField id="standard-basic" placeholder=' Search' variant="standard" className='searchs' />
               </div>
 
               <div >
@@ -45,7 +52,7 @@ function Navbar() {
                
               </div>
               <div>
-              <Typography  variant='h8' className='Username'>Username</Typography >
+              <Typography  variant='h8' className='Usernames'>Username</Typography >
               </div>
             </div>
  
@@ -54,16 +61,16 @@ function Navbar() {
 
         <Drawer anchor="left" open={open} onClose={toggleDrawer}>
 
-          <div className='leftcontent'>
+          <div className='leftcontents'>
 
-            <Link className='lnk1' to='/Dash'><Button variant='text' style={{ width: '40px'}}>DASHBOARD</Button></Link>
-            <Link className='lnk1' to='/project'><Button variant='text' style={{ width: '40px' }}>PROJECT</Button></Link>
-            <Link className='lnk1' to='/members'><Button variant='text' style={{ width: '40px' }}>MEMBER</Button></Link>
-            <Link className='lnk1'><Button variant='text' style={{ width: '40px' }}>CALENDAR</Button></Link>
-            <Link className='lnk1'><Button variant='text' style={{ width: '40px' }}>SETTINGS</Button></Link>
+            <Link className='lnks1' to='/Dash'><Button variant='text' ><img src={dashboard} className='dashlogo'></img><span className='dash-text01'>DASHBOARD</span></Button></Link>
+            <Link className='lnks2' to='/project'><Button variant='text' ><img src={project} className='dashlogo'></img><span className='dash-text01'>PROJECT</span></Button></Link>
+            <Link className='lnks3' to='/members'><Button variant='text'> <img src={member} className='dashlogo'></img><span className='dash-text01'>MEMBER</span></Button></Link>
+            <Link className='lnks4'><Button variant='text' ><img src={calendars} className='dashlogo'></img><span className='dash-text01'>CALENDAR</span></Button></Link>
+            <Link className='lnks5'><Button variant='text' ><img src={setting} className='dashlogo'></img><span className='dash-text01'>SETTINGS</span></Button></Link>
 
-            <div className='logout'>
-              <Link className='lnk1' to='/'><Button variant='text' style={{}}>LOG OUT</Button></Link>
+            <div className='logouts'>
+              <Link className='lnks1' to='/'><Button variant='text' style={{}}>LOG OUT</Button></Link>
             </div>
           </div>
 
